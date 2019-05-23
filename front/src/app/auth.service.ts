96902e32762e72e27884669b85b48ef10dd6bf89
Debrej <t.bellanger34@gmail.com>
2019-05-23 08:29:40 +0200

    Can we see the login back ?

--- front/src/app/auth.service.ts
+++ front/src/app/auth.service.ts
@@ -10,7 +10,7 @@ export class AuthService {
 
 	baseUrl = 'https://api.organify.debrej.fr';
 	connectedUser: Orga;
-	token: string = 'eaebf277e0037bc319193fe4887bc5ba6b6fabeec06e6356a93744c1e2ad0b24826c20f16983327c2c253a44f4cf8dcf';
+	token: string = 'd3fca3427b4261499745c4783272996e47cbf5786e250448fc7c35216c2ef60cb059f96e3272f1680436e84c09416999';
 
   getToken(mail, pwd){
     return this.http.post( this.baseUrl + '/login', {"mail": mail, "pwd": pwd});
